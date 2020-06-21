5536906f89d8dfca650553d239b931757e251e96
Takuho NAKANO <takotakot@users.noreply.github.com>
2020-06-21 22:06:42 +0900

    Add mockup

--- app/models/selenium_page/base.rb
+++ app/models/selenium_page/base.rb
@@ -1,2 +1,40 @@
 class SeleniumPage::Base < ApplicationRecord
+  attr_accessor :browser, :wait
+
+  CHROME_OPTIONS = Selenium::WebDriver::Chrome::Options.new(args: [
+  ].concat( Rails.env.development? ? [] : ['headless'] )
+  ).freeze
+  BROWSER_TIME_OUT = 20
+
+  def initialize(browser)
+    @browser = browser
+    @wait = Selenium::WebDriver::Wait.new(timeout: BROWSER_TIME_OUT)
+  end
+
+  def close
+    @browser.close
+    self
+  end
+
+  def quit
+    @browser.quit
+    self
+  end
+
+  def wait_css(css)
+    wait.until { @browser.find_element(css).enabled? }
+    self
+  end
+
+  def current_url
+    @browser.current_url
+  end
+
+  def click(element)
+    @browser.execute_script("arguments[0].click();", element)
+  end
+
+  def click_css(css)
+    click @browser.find_element(css)
+  end
 end
