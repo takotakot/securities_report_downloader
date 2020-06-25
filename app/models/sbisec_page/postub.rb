7bab9c3512d2d85f5a6e749e2cfb3c402d5e1427
Takuho NAKANO <takotakot@users.noreply.github.com>
2020-06-25 21:58:28 +0900

    Add mockups

--- app/models/sbisec_page/postub.rb
+++ app/models/sbisec_page/postub.rb
@@ -1,2 +1,45 @@
 class SbisecPage::Postub < SbisecPage::Base
+
+  # mock
+  def post?
+    true
+  end
+
+  # mock
+  def trash?
+    true
+  end
+
+  # mock
+  def get_line_per_page
+    20
+  end
+
+  # mock
+  def set_line_per_page
+  end
+
+  # mock
+  def go_last_page
+    self
+  end
+
+  # mock
+  def go_first_page
+    self
+  end
+
+  # mock
+  def first_page?
+    true
+  end
+
+  # mock
+  def last_page?
+    true
+  end
+
+  # TODO
+  def dl_record
+  end
 end
