9c66f3b8518c6ed59cae4d8703719c78a24cf60d
Takuho NAKANO <takotakot@users.noreply.github.com>
2020-06-28 16:46:26 +0900

    Add initial data

--- app/models/report_type.rb
+++ app/models/report_type.rb
@@ -1,2 +1,9 @@
 class ReportType < ApplicationRecord
+  HASH_DATA = [
+    {id: 1, name: '取引報告書'},
+    {id: 2, name: '取引残高報告書'},
+    {id: 3, name: '年間取引報告書'},
+    {id: 4, name: '運用報告書'},
+    {id: 5, name: 'その他'},
+  ]
 end
