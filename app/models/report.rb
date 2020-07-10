645ae607e0b62d6d8fd54cafa2ae4f91ed6fac1f
Takuho NAKANO <takotakot@users.noreply.github.com>
2020-07-11 05:32:33 +0900

    Prepare Report functions

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,24 @@
 class Report < ApplicationRecord
   belongs_to :report_type
   belongs_to :report_title
+
+  attr_accessor :manifest
+
+  def downloaded
+    return false if filename.nil?
+    return false if filename == ""
+    true
+  end
+
+  def set_file(given_filename)
+    raise unless FileTest.exist?(given_filename)
+    raise if FileTest.zero?(given_filename)
+    # TODO: type
+    self.filename= given_filename
+  end
+
+  def rename_file
+    # move file to the place where desined in the manifest
+    self
+  end
 end
