406eda6dbbbf33dffb9615ae1b825629bcb14cbf
Takuho NAKANO <takotakot@users.noreply.github.com>
2020-07-11 11:08:20 +0900

    Add support functions

--- app/models/sbisec_page/postub.rb
+++ app/models/sbisec_page/postub.rb
@@ -43,6 +43,46 @@ class SbisecPage::Postub < SbisecPage::Base
   def dl_record
   end
 
+  def message2system_id(message_element)
+    /'(\d+)'/.match(message_element.find_element(class: 'title').find_element(tag_name: 'a').attribute('onClick')).to_a[1]
+  end
+
+  def type_text2type_id(type_text)
+    type = ReportType::HASH_DATA.detect {|t| t[:name] == type_text}
+
+    if type.nil?
+      nil
+    else
+      type[:id]
+    end
+  end
+
+  def title_text2title_id(title_text)
+    title = ReportTitle::HASH_DATA.detect {|t|
+      search = '「' + t[:name] + '」'
+      ! title_text.index(search).nil?
+    }
+
+    unless title.nil?
+      title[:id]
+    else
+      unless title_text.index('運用報告書'.freeze).nil?
+        title = ReportTitle::HASH_DATA.detect {|t|
+          search = '「' + t[:name] + '」'
+          ! title_text.index(search).nil?
+        }
+      else
+
+      end
+    end
+  end
+
+  def special_title(title_text)
+    # 「ブラックロック・スーパー・マネー・マーケット・ファンド（米ドル）」運用報告書
+    # 2019年09月30日決算「ひふみプラス運用報告書」電子交付のお知らせ
+    title_text.gsub(/電子交付のお知らせ/, '')
+  end
+
   def messages
     @browser.find_element(class: 'message-list').find_elements(class: 'message')
   end
