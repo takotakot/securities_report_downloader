f0951888d783d976f8aee847ecdecf05a45e1e6d
Takuho NAKANO <takotakot@users.noreply.github.com>
2020-07-12 09:46:27 +0900

    Add upsert-like creation in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,11 @@
-report_types = ReportType.create(ReportType::HASH_DATA)
-report_titles = ReportTitle.create(ReportTitle::HASH_DATA)
+def bulk_insert(classname, data)
+  begin
+    classname.upsert_all(data)
+  rescue ArgumentError => exception
+    data.each do |item|
+      classname.find_or_create_by(item)
+    end
+  end
+end
+bulk_insert(ReportType, ReportType::HASH_DATA)
+bulk_insert(ReportTitle, ReportTitle::HASH_DATA)
